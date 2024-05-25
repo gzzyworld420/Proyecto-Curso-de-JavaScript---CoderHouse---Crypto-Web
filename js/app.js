033d79cd3e44c4b5f2eece1d17f873b4eada9955
gzzy <alejandrafuentealba@AirdeAlejandra.fibertel.com.ar>
2024-05-25 16:21:56 -0300

    agregando el header, seccion principal, logica del carrito, buscador, array de objetos, metodos de arrays utilizados, tambien agregamos dom y eventos para la proxima entrega, ahora queda hacer esto responsive y seguir con el desarrollo de las otras secciones

--- js/app.js
+++ js/app.js
@@ -1,164 +1,58 @@
-// Proyecto: Curso de JavaScript - CoderHouse - Crypto Web
+  // Proyecto: Curso de JavaScript - CoderHouse - Crypto Web
 // Autor: Ignacio Aracena - 2024
+// Variables Globales
+// Pre- entrega 03
+/*=============== Mostrar Menu ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-const container = document.getElementById('container');
-const sellBtn = document.getElementById('sell');
-const buyBtn = document.getElementById('buy');
-// Carrito de compras
-const btnCart = document.querySelector('.container-cart-icon');
-const containerCartProducts = document.querySelector('.container-cart-products');
-const cartInfo = document.querySelector('.cart-product');
-const rowProduct = document.querySelector('.row-product');
-const productsList = document.querySelector('#products');
-const valorTotal = document.querySelector('.total-pagar');
-const countProducts = document.querySelector('#contador-productos');
-const cartEmpty = document.querySelector('.cart-empty');
-const cartTotal = document.querySelector('.cart-total');
-sellBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-buyBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
-
-// Logica del Simulador Interactivo - Pre entrega 01
-// Variables globales para el saldo y criptomonedas
-let saldo = 10000;
-let bitcoin = 2;
-let ethereum = 5;
-
-const comprarCriptomoneda = () => {
-    let criptomoneda = prompt("Ingrese la criptomoneda que desea comprar (Bitcoin/Ethereum):");
-    let cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar:"));
-
-    let costoTotal = 0;
-    if (criptomoneda === "Bitcoin") {
-        const precioBitcoin = 5000;
-        costoTotal = precioBitcoin * cantidad;
-    } else if (criptomoneda === "Ethereum") {
-        const precioEthereum = 3000;
-        costoTotal = precioEthereum * cantidad;
-    } else {
-        console.log("Criptomoneda no válida.");
-        return;
-    }
-
-    if (saldo >= costoTotal) {
-        saldo -= costoTotal;
-        if (criptomoneda === "Bitcoin") {
-            bitcoin += cantidad;
-        } else {
-            ethereum += cantidad;
-        }
-        console.log(`Has comprado ${cantidad} ${criptomoneda}(s) por un total de $${costoTotal}.`);
-    } else {
-        console.log("Saldo insuficiente para realizar la compra.");
-    }
-};
-
-const venderCriptomoneda = () => {
-    let criptomoneda = prompt("Ingrese la criptomoneda que desea vender (Bitcoin/Ethereum):");
-    let cantidad = parseInt(prompt("Ingrese la cantidad que desea vender:"));
-
-    let ingresoTotal = 0;
-    if (criptomoneda === "Bitcoin") {
-        const precioBitcoin = 50000;
-        ingresoTotal = precioBitcoin * cantidad;
-    } else if (criptomoneda === "Ethereum") {
-        const precioEthereum = 3000;
-        ingresoTotal = precioEthereum * cantidad;
-    } else {
-        console.log("Criptomoneda no válida.");
-        return;
-    }
-
-    if (criptomoneda === "Bitcoin") {
-        if (bitcoin >= cantidad) {
-            saldo += ingresoTotal;
-            bitcoin -= cantidad;
-            console.log(`Has vendido ${cantidad} ${criptomoneda}(s) por un total de $${ingresoTotal}.`);
-        } else {
-            console.log("No tienes suficientes criptomonedas para vender.");
-        }
-    } else if (criptomoneda === "Ethereum") {
-        if (ethereum >= cantidad) {
-            saldo += ingresoTotal;
-            ethereum -= cantidad;
-            console.log(`Has vendido ${cantidad} ${criptomoneda}(s) por un total de $${ingresoTotal}.`);
-        } else {
-            console.log("No tienes suficientes criptomonedas para vender.");
-        }
-    }
-};
-
-const transferirCriptomoneda = () => {
-    let criptomoneda = prompt("Ingrese la criptomoneda que desea transferir (Bitcoin/Ethereum):");
-    let cantidad = parseInt(prompt("Ingrese la cantidad que desea transferir:"));
-
-    if (criptomoneda === "Bitcoin") {
-        if (bitcoin >= cantidad) {
-            bitcoin -= cantidad;
-            console.log(`Has transferido ${cantidad} ${criptomoneda}(s) con éxito.`);
-        } else {
-            console.log("No tienes suficientes criptomonedas para transferir.");
-        }
-    } else if (criptomoneda === "Ethereum") {
-        if (ethereum >= cantidad) {
-            ethereum -= cantidad;
-            console.log(`Has transferido ${cantidad} ${criptomoneda}(s) con éxito.`);
-        } else {
-            console.log("No tienes suficientes criptomonedas para transferir.");
-        }
-    } else {
-        console.log("Criptomoneda no válida.");
-    }
-};
-
-const ejecutarAccion = (accion) => {
-    switch (accion.trim()) {
-        case "comprar":
-            comprarCriptomoneda();
-            break;
-        case "vender":
-            venderCriptomoneda();
-            break;
-        case "transferir":
-            transferirCriptomoneda();
-            break;
-        default:
-            console.log(`Acción "${accion}" no válida.`);
-            break;
-    }
-};
-
-const ejecutarAcciones = () => {
-    let continuar = true;
-    while (continuar) {
-        let accion = prompt("¿Qué acción desea realizar? (comprar/vender/transferir/terminar)");
-        if (accion.toLowerCase() === "terminar") {
-            continuar = false;
-        } else {
-            ejecutarAccion(accion);
-        }
-    }
+/* Mostrar menu */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
-// Ejecutar la acción seleccionada en DevTools
-// ejecutarAcciones();
 
-// Pre entrega 02
-// Array de objetos de NFTs + Filtrado de busqueda por nombre y categoria
+/* Ocultar menu */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
+/*=============== Eliminar Menu Mobile ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== Cambiar el background del header ===============*/
+const scrollHeader = () =>{
+  const header = document.getElementById('header')
+  this.scrollY >= 50 ? header.classList.add('header-bg')
+                     : header.classList.remove('header-bg')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+// Pre-entrega 02 + 03
+/*=============== NFTs + Buscador  ===============*/
 // Recorro el array de productos para crear las cards
 for (let product of products) {
-    //Create Card
+    //Creo un div para la card
     let card = document.createElement("div");
-    //Card should have category and should stay hidden initially
+    //La clase card se agrega a todos los elementos
     card.classList.add("card", product.category, "hide");
-    //image div
+    //imagen div
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("image-container");
-    //img tag
+    //imagen tag
     let image = document.createElement("img");
     image.setAttribute("src", product.image);
     imgContainer.appendChild(image);
@@ -166,18 +60,25 @@ for (let product of products) {
     //container
     let container = document.createElement("div");
     container.classList.add("cardContainer");
-    //product name
-    let name = document.createElement("h5");
+    //nombre del nft
+    let name = document.createElement("h2");
     name.classList.add("product-name");
-    name.innerText = product.productName.toUpperCase();
+    name.innerText = product.title.toUpperCase();
     container.appendChild(name);
-    //price
+    //precio
     let price = document.createElement("p");
+    price.classList.add("price");
     price.innerText = "$" + product.price;
     container.appendChild(price);
     card.appendChild(container);
     document.getElementById("products").appendChild(card);
-    //Add to cart button
+    // Descripcion
+    let description = document.createElement("p");
+    description.classList.add("description");
+    description.innerText = product.description;
+    container.appendChild(description);
+    card.appendChild(container);
+    //Agregar boton de compra
     let button = document.createElement("button");
     button.innerText = "Add to Cart";
     button.classList.add("btn-add-cart");
@@ -186,10 +87,8 @@ for (let product of products) {
 
 // Fx para filtrar productos por categoria y nombre
 const filtrarProductos = (value) => {
-    //Button class code
     let buttons = document.querySelectorAll(".button-value");
     buttons.forEach((button) => {
-        //check if value equals innerText
         if (value.toUpperCase() == button.innerText.toUpperCase()) {
             button.classList.add("active");
         } else {
@@ -197,40 +96,29 @@ const filtrarProductos = (value) => {
         }
     });
 
-    //select all cards
     let elements = document.querySelectorAll(".card");
-    //loop through all cards
     elements.forEach((element) => {
-        //display all cards on 'all' button click
         if (value == "all") {
             element.classList.remove("hide");
         } else {
-            //Check if element contains category class
             if (element.classList.contains(value)) {
-                //display element based on category
                 element.classList.remove("hide");
             } else {
-                //hide other elements
                 element.classList.add("hide");
             }
         }
     });
 }
-
+// Evento para filtrar productos por categoria y nombre
 document.getElementById("search").addEventListener("click", () => {
-    //initializations
     let searchInput = document.getElementById("search-input").value;
     let elements = document.querySelectorAll(".product-name");
     let cards = document.querySelectorAll(".card");
 
-    //loop through all elements
     elements.forEach((element, index) => {
-        //check if text includes the search value
         if (element.innerText.includes(searchInput.toUpperCase())) {
-            //display matching card
             cards[index].classList.remove("hide");
         } else {
-            //hide others
             cards[index].classList.add("hide");
         }
     });
@@ -240,80 +128,103 @@ window.onload = () => {
     filtrarProductos("all");
 };
 
-// Carrito de compras
-let carrito = [];
+// Carrito de Compras
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
 
-// Evento para abrir y cerrar el carrito
 btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('hidden-cart');
+	containerCartProducts.classList.toggle('hidden-cart');
 });
 
-// Evento para agregar productos al carrito + Mostrar productos en el carrito
-productsList.addEventListener('click', (e) => {
-    if (e.target.classList.contains('btn-add-cart')) {
-        const product = e.target.parentElement;
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
 
-        const infoProduct = {
-            quantity: 1,
-            title: product.querySelector('h5').textContent,
-            price: product.querySelector('p').textContent,
-        };
-        // console.log(infoProduct);
-        const exists = carrito.some(product => product.title === infoProduct.title);
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('#products');
 
-        if (exists) {
-            const products = carrito.map(product => {
-                if (product.title === infoProduct.title) {
-                    product.quantity++;
-                    return product;
-                } else {
-                    return product;
-                }
-            });
-            carrito = [...products];
-        } else {
-            carrito = [...carrito, infoProduct];
-        }
-        showHTML();
-    }
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
+
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
+
+		showHTML();
+	}
 });
 
-// Evento para eliminar productos del carrito + metodo filter para comparar los productos y eliminarlos
-rowProduct.addEventListener('click', (e) => {
-    if (e.target.classList.contains('icon-close')) {
-        const product = e.target.parentElement;
-        const title = product.querySelector('p').textContent;
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
 
-        carrito = carrito.filter(
-            product => product.title !== title
-        );
-        console.log(carrito);
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
 
-        showHTML();
-    }
+		console.log(allProducts);
+
+		showHTML();
+	}
 });
 
-// Mostrar HTML del carrito + Total de productos y valor total de la compra
+// Funcion para mostrar  HTML
 const showHTML = () => {
-    if (!carrito.length) {
-        cartEmpty.classList.remove('hiddenCart');
-        rowProduct.classList.add('hiddenCart');
-        cartTotal.classList.add('hiddenCart');
-    } else {
-        cartEmpty.classList.add('hiddenCart');
-        rowProduct.classList.remove('hiddenCart');
-        cartTotal.classList.remove('hiddenCart');
-    }
-    // Limpiar HTML
-    rowProduct.innerHTML = '';
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
 
-    let total = 0;
-    let totalOfProducts = 0;
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
 
-    carrito.forEach(product => {
-        const containerProduct = document.createElement('div');
-        containerProduct.classList.add('cart-product');
-        containerProduct.innerHTML = `
+	let total = 0;
+	let totalOfProducts = 0;
+
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
             <div class="info-cart-product">
                 <span class="cantidad-producto-carrito">${product.quantity}</span>
                 <p class="titulo-producto-carrito">${product.title}</p>
@@ -334,11 +245,31 @@ const showHTML = () => {
                 />
             </svg>
         `;
-        rowProduct.append(containerProduct);
 
-        total = total + parseInt(product.quantity * product.price.slice(1));
-        totalOfProducts = totalOfProducts + product.quantity;
-    });
-    valorTotal.innerText = `$${total}`;
-    countProducts.innerText = totalOfProducts;
+		rowProduct.append(containerProduct);
+
+		total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
 };
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 2500,
+  });
+
+  sr.reveal(`.home__images`, {distance:'120px', delay: 400});
+  sr.reveal(`.home__title`, {delay: 1000});
+  sr.reveal(`.home__description`, {delay: 1200});
+  sr.reveal(`.home__button`, {delay: 1400});
+  sr.reveal(`.home__footer`, {delay: 1600});
+  sr.reveal(`.home__data div`, { origin:'right', interval: 100,delay: 1800});
+  sr.reveal(`#search-container`, {origin: 'left', interval: 100});
+  sr.reveal(`#buttons`, {origin: 'right', interval: 100});
